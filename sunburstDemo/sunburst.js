f9266db60ef44b67dbcd95bd027c3696eecae9de
Amanda Ward <amanda.ward@accenture.com>
2019-06-03 13:55:32 -0500

    no message

--- sunburstDemo/sunburst.js
+++ sunburstDemo/sunburst.js
@@ -5,14 +5,17 @@ var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
 var b = {
-  w: 200, h: 30, s: 3, t: 10
+  w: 75, h: 30, s: 3, t: 10
 };
 
 // Mapping of step names to colors.
 var colors = {
-  "Access": "#4c1d82",
-  "Quality": "#298ae6",
-  "Usage": "#13b5a3",
+  "home": "#5687d1",
+  "product": "#7b615c",
+  "search": "#de783b",
+  "account": "#6ab975",
+  "other": "#a173d1",
+  "end": "#bbbbbb"
 };
 
 // Total size of all segments; we set this later, after loading the data.
