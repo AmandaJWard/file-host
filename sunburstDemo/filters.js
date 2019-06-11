941caa816ae3e06aa1490d34ed62fa067de53858
Amanda Ward <amanda.ward@accenture.com>
2019-06-10 19:27:36 -0500

    no message

--- sunburstDemo/filters.js
+++ sunburstDemo/filters.js
@@ -2,7 +2,14 @@ d3.select("#selected-dropdown").text("first");
 
 d3.select("select")
   .on("change",function(d){
-    var selected = d3.select("#device").node().value;
+    var selected = d3.select("#subscriberType").node().value;
     console.log( selected );
-    d3.select("#selected-dropdown").text(selected);
+    d3.select("#selected-dropdown-subscriberType").text(selected);
+})
+
+d3.select("select")
+  .on("change",function(d){
+    var selected = d3.select("#location").node().value;
+    console.log( selected );
+    d3.select("#selected-dropdown-location").text(selected);
 })
